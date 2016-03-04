671e293a72c0f24d7c688b75f726f23e1b6b6f26
Kghbln <karsten@hoffmeyer.info>
2016-03-04 14:31:56 +0100

    Clean up extensions main file
    Change-Id: Idaf3c921c02343b58dcc1ed3bc0be931cd0d036f

--- UserPageEditProtection.php
+++ UserPageEditProtection.php
@@ -1,63 +1,52 @@
 <?php
-if ( ! defined( 'MEDIAWIKI' ) ) {
-   echo( "This is an extension to the MediaWiki package and cannot be run standalone.\n" );
-   die( - 1 );
-}
 /**
- * Extension: UserPageEditProtection.php
- * Created: 6 December 2007
- * Author: Lisa Ridley, Eric Gingell
- * Version: 2.0
- * Copyright (C) 2007 Lisa Ridley
+ * The UserPageEditProtection extension to MediaWiki allows to restrict the edit
+ * access to user pages.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * @link https://www.mediawiki.org/wiki/Extension:UserPageEditProtection Documentation
+ * @link https://www.mediawiki.org/wiki/Extension_talk:UserPageEditProtection Support
+ * @link https://git.wikimedia.org/summary/mediawiki%2Fextensions%2FUserPageEditProtection.git Source Code
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * @file
+ * @ingroup Extensions
+ * @package MediaWiki
  *
- * You can find a copy of the GNU General Public License at http://www.gnu.org/copyleft/gpl.html
- * A paper copy can be obtained by writing to:  Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * @author Lisa Ridley (lhridley)
+ * @author Eric Gingell
  *
- * User Page Edit Protection
- *
- * Provides User Page Edit Protection to limit user page edits to User and to sysops.
- *
- *
- * Usage:
- * Save this file in your extensions folder of your MediaWiki installation.
- * Add the following lines to LocalSettings.php:
- *    require_once('extensions/UserPageEditProtection.php');
- *    //turn on user page protection
- *    $wgOnlyUserEditUserPage = true;
- **/
-  
-/* register extension */
+ * @license https://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+// Ensure that the script cannot be executed outside of MediaWiki
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'This is an extension to MediaWiki and cannot be run standalone.' );
+}
+
+// Display extension's information on "Special:Version"
 $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
 	'name' => 'UserPageEditProtection',
-	'author' => array( 'Lisa Ridley', 'Eric Gingell' ),
-	'version' => '2.0',
+	'author' => array(
+		'Lisa Ridley',
+		'Eric Gingell',
+		'...'
+		),
+	'version' => '2.1.0-alpha',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:UserPageEditProtection',
 	'descriptionmsg' => 'userpageeditprotection-desc',
 );
 
-# Internationalisation file
-$dir = dirname(__FILE__) . '/';
-$wgExtensionMessagesFiles['UserPageEditProtection'] = $dir . 'UserPageEditProtection.i18n.php';
+// Load internationalization files
+$wgExtensionMessagesFiles['UserPageEditProtection'] = __DIR__ . '/UserPageEditProtection.i18n.php';
 
-# Define the new user right
+// Add user permission
 $wgAvailableRights[] = 'editalluserpages';
 $wgGroupPermissions['sysop']['editalluserpages'] = true;
 
-/* use the userCan hook to check user page edit permissions */
+// Register hook
 $wgHooks['userCan'][] = 'fnUserPageEditProtection';
 
+// Perform action
 function fnUserPageEditProtection( $title, $user, $action, &$result ) {
 	global $wgOnlyUserEditUserPage;
 	$lTitle = explode( '/', $title->getText() );
@@ -81,4 +70,3 @@ function fnUserPageEditProtection( $title, $user, $action, &$result ) {
 	$result = null;
 	return true;
 }
-
