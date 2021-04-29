f1ef0e3bd05c6bf6c2326810dbcf01caaa7cf29e
Sébastien Beyou <sebastien.beyou@wiki-valley.com>
2021-04-29 17:31:28 +0200

    Remove declaration of a nonexistent alias file.
    Change-Id: Ia8edcfb34f80c309f36f9c37a5a4d265382703c2

--- UserPageEditProtection.php
+++ UserPageEditProtection.php
@@ -27,8 +27,6 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'UserPageEditProtection' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['UserPageEditProtection'] = __DIR__ . '/i18n';
-	// phpcs:ignore Generic.Files.LineLength.TooLong
-	$wgExtensionMessagesFiles['UserPageEditProtectionAlias'] = __DIR__ . '/UserPageEditProtection.alias.php';
 	wfWarn(
 		'Deprecated PHP entry point used for the UserPageEditProtection extension. ' .
 		'Please use wfLoadExtension instead, ' .
