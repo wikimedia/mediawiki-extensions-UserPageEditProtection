fe1622aee76e5be3628d501c6c002253413bf919
Sam Reed <sam@reedyboy.net>
2026-01-27 19:48:34 +0000

    Modernise and simplify extension
    Change-Id: I9c78f59e0e311b2bceb6a8a848ccdd2f183bf108

--- includes/UserPageEditProtection.php
+++ includes/UserPageEditProtection.php
@@ -1,44 +1,36 @@
 <?php
+
 /**
  * UserPageEditProtection
  *
  * @file
+ *
  * @ingroup Extensions
  *
  * @license GPL-2.0-or-later
  */
 
-class UserPageEditProtection {
+namespace MediaWiki\Extension\UserPageEditProtection;
 
-	/**
-	 * @param Title $title
-	 * @param User $user
-	 * @param string $action
-	 * @param array|string|MessageSpecifier &$result
-	 *
-	 * @return bool
-	 */
-	public static function onGetUserPermissionsErrors( $title, $user, $action, &$result ) {
+use MediaWiki\Permissions\Hook\GetUserPermissionsErrorsHook;
+
+class UserPageEditProtection implements GetUserPermissionsErrorsHook {
+
+	/** @inheritDoc */
+	public function onGetUserPermissionsErrors( $title, $user, $action, &$result ) {
 		global $wgOnlyUserEditUserPage;
-		if ( !( $action == 'edit' || $action == 'move' ) ) {
-			$result = null;
-			return true;
+
+		if (
+			$wgOnlyUserEditUserPage &&
+			( $action === 'edit' || $action === 'move' ) &&
+			$title->getNamespace() === NS_USER &&
+			!$user->isAllowed( 'editalluserpages' ) &&
+			!$title->isSamePageAs( $user->getUserPage() )
+		) {
+			// TODO: This really should return a message
+			$result = false;
+			return false;
 		}
-		if ( $title->getNamespace() !== NS_USER ) {
-			$result = null;
-			return true;
-		}
-		if ( $wgOnlyUserEditUserPage ) {
-			$lTitle = explode( '/', $title->getText() );
-			if ( $user->isAllowed( 'editalluserpages' ) || ( $user->getname() == $lTitle[0] ) ) {
-				$result = null;
-				return true;
-			} else {
-				$result = false;
-				return false;
-			}
-		}
-		$result = null;
 		return true;
 	}
 }
